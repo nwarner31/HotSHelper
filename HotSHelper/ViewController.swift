4610689267ff74150e50ffada9bf0ac6127e9d58
Nathaniel Warner <natedawg3124@gmail.com>
2018-03-28 16:45:11 -0700

    Initial Commit

---
+++ HotSHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotSHelper
+//
+//  Created by Nathaniel Warner on 3/28/18.
+//  Copyright © 2018 Nathaniel Warner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
